88187750bc6f7547857c94ad7e3890714770461e
Wiki Wookie <westleyisbell@gmail.com>
2020-11-10 18:58:11 -0600

    Add settings route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 import { AboutComponent } from './pages/about/about.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 import { TodoComponent } from './pages/todo/todo.component';
 
 const routes: Routes = [
   { path: '', component: AboutComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'settings', component: SettingsComponent },
   { path: 'todo/list', component: TodoComponent, canActivate: [AuthGuard] },
 ];
 
